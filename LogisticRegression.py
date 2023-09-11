7fa312220f4c4bb776d95075916aad5ee398c56e
Tamir <tamir303@gmail.com>
2023-09-11 14:28:31 +0300

    LogisticRegression finished Binom+Multi, no regularization yet

--- LogisticRegression.py
+++ LogisticRegression.py
@@ -1,11 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 class LogisticRegression:
     def __init__( self,
-                  epochs: int = 20,
-                  learning_rate: float = 0.0001,
+                  epochs: int = 100,
+                  learning_rate: float = 0.01,
                   threshold: float = 0.5):
         """
         Initialize the LinearRegression model.
@@ -20,76 +19,125 @@ class LogisticRegression:
             'ep': epochs,
             'lr': learning_rate,
             'th': threshold,
-            'wi': None,
+            'w': None,
             'b': None,
             'stra': ''
         }
 
-    def fit( self, X, y ):
-        if self.params['wi'] is None:
-            num_features = 1 if isinstance(X[ 0 ], (float, int)) else len(X[ 0 ])
-            # initialize random parameters
-            self.params[ 'wi' ] = np.random.uniform(0, 1, size=num_features) * 1
-            self.params[ 'b' ] = np.random.uniform(0, 1) * 1
-            self.params[ 'stra' ] = 'bin' if len(set(y)) == 2 else 'mul'
+    def fit( self, X, y, verbose=True ):
+        def binomial():
+            self.params[ 'w' ] = np.random.normal(loc=0.0, scale=std, size=n_features) * 1
+            self.params[ 'b' ] = np.random.normal(loc=0.0, scale=std) * 1
+
+        def multinomial():
+            self.params[ 'w' ] = np.random.normal(loc=0.0, scale=std, size=(n_features, n_classes)) * 1
+            self.params[ 'b' ] = np.random.normal(loc=0.0, scale=std, size=n_classes) * 1
+
+        if self.params['w'] is None:
+            std = 1e-3
+            n_features = X.shape[ 1 ]  # number of features
+            n_classes = len(np.unique(y))  # number of classes in the dataset
+            strategy = self.params[ 'stra' ] = 'bin' if n_classes == 2 else 'mul'
+            binomial() if strategy == 'bin' else multinomial()
 
         epochs = self.params['ep']
         x = np.array(X)
         y = np.array(y)
 
         for epoch in range(epochs):
-            predictions = self.predict(X)
+            predictions = self.predict(x, prob=True)
             gradients = self.__backward_propagation(x, y, predictions)
             self.__update_params(gradients)
-            print(f"Epoch {epoch + 1}: \tLoss {self.cost_function(predictions, y):.2f}")
+            if verbose:
+                print(f"Epoch {epoch + 1}: \tLoss {self.cost_function(predictions, y):.2f}")
 
-    def predict( self, X ):
-        w = self.params[ 'wi' ]
+    def predict( self, X, prob=False ):
+        w = self.params[ 'w' ]
         b = self.params[ 'b' ]
-        threshold = self.params['th']
-        strategy = self.params['stra']
         if w is not None and b is not None:
             x = np.array(X)
             z = x @ w + b
-            if strategy == 'bin':
-                # Sigmoid
-                predictions = np.array([1 if self.__sigmoid(i) >= threshold else 0 for i in z])
-            else:
-                # Softmax
-                predictions = np.array([self.__softmax(z, k) for k in range(x.shape[0]) ])
-            return predictions
+            return self.__predictor(z, prob)
         raise Exception('Model not fitted')
-
-    def cost_function( self, predictions, y ):
-        e = 1e-15  # Small epsilon value to avoid taking the log of zero
-        ones = np.ones(len(y))
-        cost = -(np.sum(y * np.log(predictions + e) + (ones - y) * np.log(ones - predictions + e)))
-        return cost
-
-    def log_likelihood_function( self, predictions, y ):
-        return -1 * self.cost_function(predictions, y)
 
     def __backward_propagation( self, x, y, predictions ):
         gradients = {}
-        df = (y - predictions) * -1
-        dw = np.array([np.sum(np.multiply(df, x[:, col])) for col in range(x.shape[1])])
-        db = np.sum(df)
+        strategy = self.params['stra']
+        n_samples = y.shape[ 0 ]
+        dscores = predictions.copy()
+
+        if strategy == 'bin':
+            df = (y - dscores) * -1
+            dw = np.array([ np.sum(np.multiply(df, x[ :, col ])) for col in range(x.shape[ 1 ]) ])
+            db = np.sum(df)
+        else:
+            dscores[ np.arange(n_samples), y ] -= 1
+            dscores /= n_samples
+            dw = x.T.dot(dscores)
+            db = np.sum(dscores, axis=0, keepdims=True)
+
         gradients[ 'dw' ] = dw
         gradients[ 'db' ] = db
         return gradients
 
-    def __sigmoid( self, z ):
-        return 1 / (1 + np.exp(-z))
+    def __predictor( self, z, prob ):
+        def sigmoid():
+            return 1 / (1 + np.exp(-z))
 
-    def __softmax( self, z, k):
-        return np.exp(z[k]) / np.sum(np.exp(z))
+        def softmax():
+            exp_scores = np.exp(z - np.max(z, axis=1, keepdims=True))
+            return exp_scores / np.sum(exp_scores, axis=1, keepdims=True)
+
+        strategy = self.params['stra']
+        if strategy == 'bin':
+            score = sigmoid()
+            return (score >= self.params['th']).astype(int) if not prob else score
+        else:
+            score = softmax()
+            return np.argmax(score, axis=1) if not prob else score
+
+    def cost_function( self, predictions, y ):
+        strategy = self.params[ 'stra' ]
+        n_samples = y.shape[ 0 ]  # number of samples
+        if strategy == 'bin':
+            e = 1e-15  # Small epsilon value to avoid taking the log of zero
+            ones = np.ones(n_samples)
+            cost = -(np.sum(y * np.log(predictions + e) + (ones - y) * np.log(ones - predictions + e))) / n_samples
+        else:
+            corr_logprobs = -np.log(predictions[ np.arange(n_samples), y ])
+            cost = np.sum(corr_logprobs)
+
+        return cost
 
     def __update_params( self, gradients ):
         grad_b = gradients[ 'db' ]
         grad_w = gradients[ 'dw' ]
         lr = self.params[ 'lr' ]
-        w = self.params[ 'wi' ]
+        w = self.params[ 'w' ]
         b = self.params[ 'b' ]
         self.params[ 'b' ] = b - lr * grad_b
-        self.params[ 'wi' ] = w - lr * grad_w
+        self.params[ 'w' ] = w - lr * grad_w
 
+    def plot2D( self, X, y ):
+        if X.shape[ 1 ] != 2:
+            raise ValueError("Plotting is only supported for 2D feature data.")
+
+        # Generate a grid of points for plotting the decision boundary
+        x_min, x_max = X[ :, 0 ].min() - 1, X[ :, 0 ].max() + 1
+        y_min, y_max = X[ :, 1 ].min() - 1, X[ :, 1 ].max() + 1
+        xx, yy = np.meshgrid(np.arange(x_min, x_max, 0.01), np.arange(y_min, y_max, 0.01))
+
+        # Predict the class labels for the grid points
+        grid_points = np.c_[ xx.ravel(), yy.ravel() ]
+        Z = self.predict(grid_points)
+
+        # Reshape the predictions to the meshgrid shape
+        Z = Z.reshape(xx.shape)
+
+        # Create a contour plot to visualize the decision boundary
+        plt.contourf(xx, yy, Z, cmap=plt.cm.RdBu, alpha=0.8)
+        plt.scatter(X[ :, 0 ], X[ :, 1 ], c=y, cmap=plt.cm.RdBu, edgecolors='k')
+        plt.xlabel('Feature 1')
+        plt.ylabel('Feature 2')
+        plt.title('Decision Boundary')
+        plt.show()
--- main.py
+++ main.py
@@ -1,30 +1,24 @@
 from sklearn.model_selection import train_test_split
-from sklearn import datasets, metrics
+from sklearn.datasets import make_classification
 from LogisticRegression import LogisticRegression
 
-# load the digit dataset
-digits = datasets.load_digits()
-
-# defining feature matrix(X) and response vector(y)
-X = digits.data
-y = digits.target
-
+# Generate a synthetic binary classification dataset with two informative features
+X, y = make_classification(
+    n_samples=10000,  # Number of samples
+    n_features=2,  # Number of features
+    n_informative=2,  # Number of informative features
+    n_redundant=0,  # Number of redundant features
+    n_clusters_per_class=1,  # Number of clusters per class
+    random_state=29  # Random seed for reproducibility
+)
 # splitting X and y into training and testing sets
-X_train, X_test,\
-	y_train, y_test = train_test_split(X, y,
-									test_size=0.4,
-									random_state=1)
-
+X_train, X_test, \
+    y_train, y_test = train_test_split(X, y,
+                                       test_size=0.4,
+                                       random_state=1)
 # create logistic regression object
-reg = LogisticRegression()
+reg = LogisticRegression(epochs=100)
 
 # train the model using the training sets
 reg.fit(X_train, y_train)
-
-# making predictions on the testing set
-y_pred = reg.predict(X_test)
-
-# comparing actual response values (y_test)
-# with predicted response values (y_pred)
-print("Logistic Regression model accuracy(in %):",
-	metrics.accuracy_score(y_test, y_pred)*100)
+reg.plot2D(X_test, y_test)
